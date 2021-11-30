f9bab0984615bfe65b5be64edebc2227e039e8e4
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2021-11-30 16:36:34 +0300

    Updated gitignore

--- cognition/cognition/settings.py
+++ cognition/cognition/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "core.apps.CoreConfig",
+    "core",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -78,8 +78,8 @@ WSGI_APPLICATION = "cognition.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.spatialite",
+        "NAME": BASE_DIR / "db.spatialite",
     }
 }
 
