bf3ee05411261ef9819b004b87e519e93dc26a93
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2021-12-09 17:15:22 +0300

    Added UUID as ID

--- cognition/core/admin.py
+++ cognition/core/admin.py
@@ -1,13 +1,20 @@
 from django.contrib.gis import admin
 
-from .models import Device, Location, Sensor
-
-admin.site.register(Device)
-admin.site.register(Sensor)
+from .models import Device, Location, Sensor, SensorRecord
 
 
+class UUIDAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+
+# Needed for better map preview
 class LocationAdmin(admin.OSMGeoAdmin):
-    pass
+    readonly_fields = ("id",)
+
+
+admin.site.register(Device, UUIDAdmin)
+admin.site.register(Sensor, UUIDAdmin)
+admin.site.register(SensorRecord)
 
 
 admin.site.register(Location, LocationAdmin)
--- cognition/core/models.py
+++ cognition/core/models.py
@@ -1,8 +1,17 @@
+import uuid
+
 from django.contrib.gis.db import models as gis_models
 from django.db import models
 
 
-class Location(models.Model):
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Location(UUIDModel):
     label = models.CharField(max_length=120)
     coordinate = gis_models.PointField()
 
@@ -10,7 +19,7 @@ class Location(models.Model):
         return self.label
 
 
-class Device(models.Model):
+class Device(UUIDModel):
     label = models.CharField(max_length=120)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
@@ -18,7 +27,7 @@ class Device(models.Model):
         return self.label
 
 
-class Sensor(models.Model):
+class Sensor(UUIDModel):
     INTERNAL = "int"
     EXTERNAL = "ext"
     USAGE_CHOICES = [(INTERNAL, "Internal"), (EXTERNAL, "External")]
@@ -50,3 +59,6 @@ class SensorRecord(models.Model):
     value = models.FloatField()
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.timestamp} {self.sensor}"
