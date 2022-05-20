7329b49d96c37f2e84d1e2bb69e5924743e5efad
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2022-05-20 12:41:07 +0300

    Added cut for data (last hour), updated compose

--- src/nft_generator/views.py
+++ src/nft_generator/views.py
@@ -1,13 +1,12 @@
 # import csv
 # from datetime import datetime
+from datetime import datetime
 from io import StringIO
 from urllib.parse import urljoin
 
 import pandas as pd
 import requests
 from django.conf import settings
-
-# from api.models import Location, Sensor, SensorRecord
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -16,14 +15,21 @@ from .forms import GenerateNFTForm
 # from django.utils.timezone import make_aware
 
 
+def get_last_hour():
+    now = datetime.now()
+    return now.strftime("%Y%m%d%H0000")
+
+
 def generate_nft(request):
     if request.method == "POST":
         form = GenerateNFTForm(request.POST)
         if form.is_valid():
             api_key = settings.NFT_GENERATOR_API_KEY
             sensor_name = settings.NFT_GENERATOR_SENSOR_NAME
+            from_date = get_last_hour()
+
             res = requests.get(
-                f"https://www.irrimaxlive.com/api/?cmd=getreadings&key={api_key}&name={sensor_name}"
+                f"https://www.irrimaxlive.com/api/?cmd=getreadings&key={api_key}&name={sensor_name}&from={from_date}"
             )
 
             print(res.text)
@@ -32,6 +38,8 @@ def generate_nft(request):
 
             df.drop("Date Time", inplace=True, axis=1)  # Removing date column
             df.drop(df[df["A1(5)"] < 0].index, inplace=True)  # Removing -1 rows
+
+            # df.to_csv("test.csv", index=False)
 
             host_url = settings.HOST_URL
 
